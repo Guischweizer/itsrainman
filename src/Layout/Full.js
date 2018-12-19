f90c08503a812ee09e3d5d70c346e5acd7b29f59
GuilhermeSchweizer <guilherme.schweizer@gmail.com>
2018-12-19 16:55:13 -0200

    Removed some extra files

--- src/Layout/Full.js
+++ src/Layout/Full.js
@@ -1,12 +1,30 @@
 import React, { Component } from "react";
+import { Redirect } from "react-router-dom";
+
 import snow from "../assets/images/snow.svg";
 import "../assets/stylesheet/Full.css";
 
 class Full extends Component {
+    constructor() {
+        super();
+        this.state = {
+            previsao: false
+        };
+    }
     render() {
+        const { previsao } = this.state;
+        if (previsao) {
+            return <Redirect to="/previsao" />;
+        }
         return (
             <div className="body-content">
-                <button type="button" className="btn button-previsao ">
+                <button
+                    type="button"
+                    className="btn button-previsao"
+                    onClick={() => {
+                        this.setState({ previsao: true });
+                    }}
+                >
                     <div className="col-md-12">
                         <img
                             src={snow}
